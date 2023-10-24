39cbd00fbabb4dcfb48c546ecc50468c316f9ab6
Akbar Maulana <akbar.maulana26061999@gmail.com>
2023-10-24 15:15:34 +0700

    commit

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/welcome', function () {
-    return view('welcome');
+Route::get('/', function () {
+    return view('ui.dashboard');
 });
+
+Route::get('/welcome', function () {
+    return view('ui.slide.index');
+});
+
